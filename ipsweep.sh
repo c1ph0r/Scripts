bdd7cea25f642ae68fe2c1a5eb9531fda92d97be
Edward Miro <c1ph0r@protonmail.com>
2020-06-07 11:59:10 -0700

    Update ipsweep.sh

--- ipsweep.sh
+++ ipsweep.sh
@@ -1,12 +1,13 @@
 # Copied from [Practical Ethical Hacking - The Complete Course]
+# Slightly modified to output results to file and add timestamp to log name.
 #!/bin/bash
 if [ "$1" == "" ]
 then
 echo "You forgot an IP address!"
 echo "Syntax: ./ipsweep.sh 192.168.1"
-
 else
+TIMESTAMP=`date +%Y-%m-%d_%H-%M-%S`
 for ip in `seq 1 254`; do
-ping -c 1 $1.$ip | grep "64 bytes" | cut -d " " -f 4 | tr -d ":" &
+ping -c 1 $1.$ip | grep "64 bytes" | cut -d " " -f 4 | tr -d ":" >> scanlog$TIMESTAMP &
 done
 fi
